fa8d03e54fe260f7210678850d7ef2b3ab4432fc
mohitkmeena <mohitmeenaG2005@gmail.com>
2025-04-01 01:23:29 +0530

    auth-service initialized

---
+++ src/main/java/com/mohit/invoice_financing_auth_service/InvoiceFinancingAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mohit.invoice_financing_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceFinancingAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceFinancingAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohit/invoice_financing_auth_service/InvoiceFinancingAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohit.invoice_financing_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceFinancingAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
